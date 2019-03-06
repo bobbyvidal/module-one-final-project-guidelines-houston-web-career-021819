fa14208e36242c3cf2ce78d392d7d062b0683018
HemalathaMurugan <hema@Hemalathas-MacBook-Pro.local>
2019-03-06 11:01:19 -0600

    Just the methods and their outlines

--- lib/models/restaurant.rb
+++ lib/models/restaurant.rb
@@ -2,5 +2,11 @@ class Restaurant < ActiveRecord::Base
 
     has_many :reviews
     has_many :customers, through: :reviews
+    def name
+       #returns the name of that particular restaurant instance
+    end
 
+    def reviews
+       #returns all the reviews received from customers
+    end
 end
--- lib/models/review.rb
+++ lib/models/review.rb
@@ -2,19 +2,34 @@ class Review < ActiveRecord::Base
     
     belongs_to :restaurant
     belongs_to :customer
+    def write_review
+        # a method to make a customer instance to write a review for a restaurant
+    end
+ 
+    def search_restaurant
+        # returns the top 5 rated restaurant in the favorite cuisine of that customer
+    end
+ 
+    def name
+        #simply returns the name of a customer instance
+    end
+ 
+    def favorite_cuisine
+        #simply returns the favorite cuisine of a customer instance
+    end
     
-    #validates :rating, :inclusion => {:in => 1..5}
-    if rating > 5
-        rating ==5
-    elsif rating < 1
-        rating ==1
-    end
+    # #validates :rating, :inclusion => {:in => 1..5}
+    # if rating > 5
+    #     rating ==5
+    # elsif rating < 1
+    #     rating ==1
+    # end
 
-    if affordability > 5
-        affordability ==5
-    elsif affordability < 1
-        affordability ==1
-    end
+    # if affordability > 5
+    #     affordability ==5
+    # elsif affordability < 1
+    #     affordability ==1
+    # end
 
 
     
