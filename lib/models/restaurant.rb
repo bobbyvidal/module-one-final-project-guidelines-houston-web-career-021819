d4b4fdaf61ff38d1af9400da7f250128cd63eb89
github username <github email address>
2019-03-06 11:18:17 -0600

    added specific files for methods

--- lib/models/restaurant.rb
+++ lib/models/restaurant.rb
@@ -2,11 +2,6 @@ class Restaurant < ActiveRecord::Base
 
     has_many :reviews
     has_many :customers, through: :reviews
-    def name
-       #returns the name of that particular restaurant instance
-    end
+    
 
-    def reviews
-       #returns all the reviews received from customers
-    end
 end
--- lib/models/review.rb
+++ lib/models/review.rb
@@ -2,22 +2,7 @@ class Review < ActiveRecord::Base
     
     belongs_to :restaurant
     belongs_to :customer
-    def write_review
-        # a method to make a customer instance to write a review for a restaurant
-    end
  
-    def search_restaurant
-        # returns the top 5 rated restaurant in the favorite cuisine of that customer
-    end
- 
-    def name
-        #simply returns the name of a customer instance
-    end
- 
-    def favorite_cuisine
-        #simply returns the favorite cuisine of a customer instance
-    end
-    
     # #validates :rating, :inclusion => {:in => 1..5}
     # if rating > 5
     #     rating ==5
